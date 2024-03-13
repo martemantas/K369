e71184b66d2f0e27b8d10e1bca71306de1bc1469
PatrikasA <45782374+PatrikasA@users.noreply.github.com>
2024-03-13 11:02:20 +0200

    Added new login, modified look

--- K369/.idea/.idea.K369/.idea/sonarlint/securityhotspotstore/index.pb
+++ K369/.idea/.idea.K369/.idea/sonarlint/securityhotspotstore/index.pb
@@ -7,3 +7,11 @@ R
 "Assets/TogglePasswordVisibility.cs,0\3\03992807805b0997cc7aebb287b37ebde58ec933
 O
 Assets/Scripts/NutritionPage.cs,d\1\d1fa3c696befc9355c1ed5775619524b25005ddb
+N
+Assets/FacebookLoginManager.cs,5\a\5ae127098f6d2499cfe7a1eaa1f5dbbf61bf5ec8
+K
+Assets/Scripts/LoginForm.cs,3\e\3eff25512f7b47fbc032da02f9b1f970c4790839
+N
+Assets/Scripts/RegisterForm.cs,9\b\9b9dbb71cb3b451621dc64ab6e542a41d0728ba8
+Z
+*Assets/Plugins/Android/AndroidManifest.xml,c\1\c18ebe23595b3e06d6bfdea04659b986054c6a81
---
+++ K369/Assets/Scripts/FacebookLoginManager.cs
@@ -0,0 +1,120 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Facebook.Unity;
+using System;
+using UnityEngine.SceneManagement;
+
+public class FacebookLoginManager : MonoBehaviour
+{
+    public DatabaseManager databaseManager;
+    public string MainScreenName = "Main screen";
+    public string RegisterScreenName = "Register screen";
+
+    void Awake()
+    {
+        if (!FB.IsInitialized)
+        {
+            FB.Init(InitCallback);
+        }
+        else
+        {
+            FB.ActivateApp();
+        }
+    }
+
+    private void InitCallback()
+    {
+        if (FB.IsInitialized)
+        {
+            FB.ActivateApp();
+        }
+        else
+        {
+            Debug.Log("Failed to Initialize the Facebook SDK");
+        }
+    }
+
+    public void FacebookLogin()
+    {
+        var permissions = new List<string>() { "public_profile", "email" };
+        FB.LogInWithReadPermissions(permissions, AuthCallback);
+    }
+
+    private void AuthCallback(ILoginResult result)
+    {
+        if (FB.IsLoggedIn)
+        {
+            var aToken = AccessToken.CurrentAccessToken;
+            FB.API("me?fields=id,email,name", HttpMethod.GET, FacebookAPICallback);
+        }
+        else
+        {
+            Debug.Log("User cancelled login");
+        }
+    }
+
+    private void FacebookAPICallback(IGraphResult result)
+    {
+        if (result.Error != null)
+        {
+            Debug.LogError("Error getting Facebook user info: " + result.Error);
+            return;
+        }
+
+        // Parse the user's Facebook data
+        var id = result.ResultDictionary["id"].ToString();
+        var email = result.ResultDictionary.ContainsKey("email") ? result.ResultDictionary["email"].ToString() : "";
+        var name = result.ResultDictionary["name"].ToString();
+
+        // Check if the user exists in your database
+        StartCoroutine(CheckUserInDatabase(email, id, name));
+    }
+
+    private IEnumerator IsEmailUnique(string email, Action<bool> callback)
+    {
+        WaitForCallback wait = new WaitForCallback();
+        bool isUnique = false;
+
+        databaseManager.GetUserByEmail(email, (User user) =>
+        {
+            isUnique = user == null;
+            wait.Complete(); // Indicate that the callback has been called
+        });
+
+        yield return wait;
+
+        callback(isUnique);
+    }
+    private IEnumerator CheckUserInDatabase(string email, string facebookId, string name)
+    {
+        bool emailNotRegistered = false;
+        yield return StartCoroutine(IsEmailUnique(email, notExists => {
+            emailNotRegistered = notExists;
+        }));
+
+        if (emailNotRegistered)
+        {
+            SceneManager.LoadScene(RegisterScreenName);
+
+            yield break;
+        }
+        LoadScene();
+    }
+
+    public void LoadScene()
+    {
+        SceneManager.LoadScene(MainScreenName);
+    }
+
+    public class WaitForCallback : CustomYieldInstruction
+    {
+        private bool isDone = false;
+
+        public override bool keepWaiting => !isDone;
+
+        public void Complete()
+        {
+            isDone = true;
+        }
+    }}
