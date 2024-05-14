114955707f9214586131be93b5febe240088c808
PatrikasA <45782374+PatrikasA@users.noreply.github.com>
2024-05-14 23:41:21 +0300

    Added validation

--- K369/Assets/Scripts/InstantiatePrefab.cs
+++ K369/Assets/Scripts/InstantiatePrefab.cs
@@ -8,14 +8,36 @@ public class InstantiatePrefab : MonoBehaviour
     public GameObject prefabToInstantiate;
     public Transform prefabParent;
     public TMP_InputField IdInput;
+    public TMP_Text errorMessageText;
+    public GameObject modal;
+    public ChildController childController;
 
     public void InstantiatePrefabMethod()
     {
-        DatabaseManager.Instance.AddChildToUser(UserManager.Instance.CurrentUser.Id, int.Parse(IdInput.text));
-        if (UserManager.Instance == null)
+        int enteredChildId = int.Parse(IdInput.text);
+
+        if (UserManager.Instance.CurrentUser.children.Contains(enteredChildId))
+        {
+            errorMessageText.text = "Incorrect ID";
             return;
-        UserManager.Instance.CurrentUser.children.Add(int.Parse(IdInput.text));
-        GameObject instantiatedPrefab = Instantiate(prefabToInstantiate, prefabParent ? prefabParent : null);
-        IdInput.text = "";
+        }
+
+        DatabaseManager.Instance.CheckIfChildIdExists(enteredChildId, exists =>
+        {
+            if (exists)
+            {
+                DatabaseManager.Instance.AddChildToUser(UserManager.Instance.CurrentUser.Id, enteredChildId);
+                UserManager.Instance.CurrentUser.children.Add(enteredChildId);
+                GameObject instantiatedPrefab = Instantiate(prefabToInstantiate, prefabParent ? prefabParent : null);
+                IdInput.text = "";
+                errorMessageText.text = "";
+                modal.SetActive(false);
+                childController.OnAddButton();
+            }
+            else
+            {
+                errorMessageText.text = "Incorrect ID";
+            }
+        });
     }
 }
